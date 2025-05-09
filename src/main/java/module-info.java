adf8a1b366601436617f820a2e9c441a267c50eb
Félix Blanchier <felix.blanchier@gmail.com>
2025-05-09 16:20:16 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module projet.projetjava {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens projet.projetjava to javafx.fxml;
+    exports projet.projetjava;
+}
