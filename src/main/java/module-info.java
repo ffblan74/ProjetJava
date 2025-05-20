52e4ac7c9f75c592a3dcc2515887f0c90540c2b5
Danré <hippolyte.danre@gmail.com>
2025-05-20 12:36:08 +0200

    Ajout Création de cours pour prof

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,24 @@
 module projet.projetjava {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires org.apache.commons.csv;
 
-    opens projet to javafx.fxml;
     exports projet;
     exports projet.controleurs;
-    opens projet.controleurs to javafx.fxml;
-    exports projet.models;
-    opens projet.models to javafx.fxml;
     exports projet.controleurs.admin;
-    opens projet.controleurs.admin to javafx.fxml;
+    exports projet.controleurs.professeur; // <<-- Assurez-vous que c'est là
+    exports projet.models;
     exports projet.utils;
+
+    opens projet to javafx.fxml;
+    opens projet.controleurs to javafx.fxml;
+    opens projet.controleurs.admin to javafx.fxml;
+    opens projet.controleurs.professeur to javafx.fxml; // <<-- Assurez-vous que c'est là
+    opens projet.models to javafx.fxml, javafx.base;
     opens projet.utils to javafx.fxml;
 }
